cdadfbdd3da4382c3d55f813e1f740298ebc6d47
unknown <lakis004@umn.edu>
2018-09-22 20:03:31 -0500

    final

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,22 +1,118 @@
-import React from "react";
+import React, { Component } from "react";
 import FamilyGuy from "./components/FamilyGuy";
+import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+//import Column from "./Column";
 import friends from "./friends.json";
 import "./App.css";
 
-const App = () => (
-  <Wrapper>
-    <h1 className="title">Play The Family Guy Clicky Game</h1>
-    <FamilyGuy
-      name={friends[0]}
-    />
-    <FamilyGuy
-      name={friends[1]}
-    />
-    <FamilyGuy
-      name={friends[2]}
-    />
-  </Wrapper>
-);
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
+class App extends Component {
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: []
+  };
+
+  handleClick = id => {
+    //event.preventDefault();
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.initiateReset();
+    }
+  };
+
+  handleIncrement = () => {
+    //event.preventDefault();
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    } 
+     if (newScore === 14) {
+     this.winnerReset();
+    };
+    this.initiateShuffle();
+  };
+
+  winnerReset = () => {
+    //event.preventDefault();
+    this.setState({
+      currentScore: 0,
+      rightWrong: "You won, you're pretty smart!",
+      clicked: []
+    });
+    this.initiateShuffle();
+  };
+
+  initiateReset = () => {
+    //event.preventDefault();
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Hehehehe You Lost Stupid!",
+      clicked: []
+    });
+    this.initiateShuffle();
+  };
+
+  initiateShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+
+         <Nav
+          title="Family Guy Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Title className="titleMessage">
+          Try to click on each character without hitting the same person twice!!!
+        </Title>
+       
+        <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              // <Column size="md-3">
+                <FamilyGuy
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  initiateReset={this.initiateReset}
+                  initiateShuffle={this.initiateShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              // </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
 export default App;
--- clicky-game/src/components/FamilyGuy.js
+++ clicky-game/src/components/FamilyGuy.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./FamilyGuy.css";
 
 const FamilyGuy = props => (
-    <div className="card">
-        <div className="img-container">
-            <img alt={props.name} src={props.image} />
-        </div>
+  <div 
+    className="card" 
+    value={props.id} 
+    onClick={() => props.handleClick(props.id)}
+  >
+    <div className="img-container hvr-wobble-to-bottom-right">
+      <img style={{width: 80, height: 145}}alt={props.name} src={props.image} />
     </div>
+  </div>
 );
 
 export default FamilyGuy;
